c169bdcd2613c1f1c92cca57c3522a5684309d3e
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-11-11 11:55:48 +0200

    implement register handler

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import userRouter from "./routes/userRouter.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+if (process.env === "development") {
+  console.log(morgan("dev"));
+}
 app.use("/api/user", userRouter);
 
 export default app;
--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -4,4 +4,9 @@ export const register = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
   const user = await User.create({ firstName, lastName, email, password });
+
+  res.status(201).send({
+    success: true,
+    user,
+  });
 };
