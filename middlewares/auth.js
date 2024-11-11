59b9414a698050b68b541aa58ea1194471b27872
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-11-11 18:40:20 +0200

    implement authorization middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import CustomError from "./../utils/CustomError.js";
+import asyncErrorHandler from "../utils/asyncErrorHandler.js";
+import { User } from "../models/User.js";
+
+const auth = asyncErrorHandler(async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split(" ")[1];
+
+  // * check if token exist
+  if (!token) {
+    return next(new CustomError("Authentication token missing.", 401));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_STR);
+
+  const user = await User.findById({ _id: decoded._id });
+  // * check if user for given token exist
+  if (!user) {
+    return next(
+      new CustomError(
+        "User associated with the token doest not exist. Please login again",
+        401
+      )
+    );
+  }
+
+  req.user = user;
+
+  next();
+});
+
+export default auth;
