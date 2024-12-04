000bd449704ce8de05907aed262fcde2921ad4cf
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-12-04 20:00:32 +0200

    Change cors configs

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import catchAllRoutes from "./middlewares/catchAllRoutes.js";
 import userRouter from "./routes/userRouter.js";
 import authRouter from "./routes/authRouter.js";
 import invoiceRouter from "./routes/invoiceRouter.js";
-
+ 
 const app = express();
 
 const limiter = rateLimit({
@@ -20,11 +20,8 @@ const limiter = rateLimit({
     "We have receive too many request from this IP Address. Please try again in one hour.",
 });
 
-const corsOptions = {
-  origin: process.env.FRONTEND_URL,
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(sanitize());
